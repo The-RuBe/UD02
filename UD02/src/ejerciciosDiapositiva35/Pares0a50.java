bf7c808907148670d9a7e5f542a737cba93b8b50
The-RuBe <rubenroldanjimenez12@gmail.com>
2025-11-10 13:44:00 +0100

    Estoy cansado jefe

---
+++ UD02/src/ejerciciosDiapositiva35/Pares0a50.java
@@ -0,0 +1,16 @@
+package ejerciciosDiapositiva35;
+
+public class Pares0a50 {
+
+	public static void main(String[] args) {
+		
+		int a=0;
+		
+		while (a<=50) {
+			System.out.println(a);
+			a+=2;
+		}
+
+	}
+
+}
