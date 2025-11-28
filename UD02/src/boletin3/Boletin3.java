7f7b17d6ab5643dcb8d9dd660ba96ee1144cd9a9
The-RuBe <rubenroldanjimenez12@gmail.com>
2025-11-28 13:46:31 +0100

    Add comments and improve formatting in Boletin3
    Added explanatory comments and improved code formatting in Boletin3.java for better readability and maintainability. Minor indentation adjustment in Main.java.

--- UD02/src/boletin3/Boletin3.java
+++ UD02/src/boletin3/Boletin3.java
@@ -4,28 +4,36 @@ import java.util.Scanner;
 
 public class Boletin3 {
 
+	// Se crea el método estático del ejercicio 1
 	public static void Ejercicio1() {
+		// Se crea el Scanner
 		Scanner sc = new Scanner(System.in);
 
 		int numero;
 		int aux;
 
 		do {
+			// Se introduce el número
 			System.out.print("Introduzca un número entero positivo: ");
 			numero = sc.nextInt();
-		} while (numero < 0);
+		} while (numero < 0); // El número debe ser mayor que 0 para salir del bucle
 
+		// Se integran las variables
 		aux = numero;
 		int vuelta = 0;
 
-		while (numero > 0) {
+		while (numero > 0) { // Mientras el número sea mayor que 0 el bucle se sigue ejecutando
+			// Se le da la vuelta al número para luego
 			vuelta = (vuelta * 10) + (numero % 10);
 			numero = numero / 10;
 		}
 
+		// Se muestra por pantalla el mensaje anterior al número para luego mostrarlo
 		System.out.printf("El número %d en palotes es: ", aux);
 
-		while (vuelta > 0) {
+		while (vuelta > 0) { // Se hace el bucle para sacar los palos con el número invertido para que lo
+								// proporcione bien
+
 			int digito = vuelta % 10;
 
 			for (int i = 0; i < digito; i++) {
@@ -33,27 +41,30 @@ public class Boletin3 {
 			}
 
 			vuelta = vuelta / 10;
-			if (vuelta > 0) {
+			if (vuelta > 0) { // Esta condición se realiza para no poner un guión al final
 				System.out.print("-");
 			}
 		}
 
-		System.out.println();
-
-		sc.close();
+		sc.close(); // Se cierra el Scanner
 	}
 
+	// Se crea el método estático del ejercicio 2
 	public static void Ejercicio2() {
-
+		// Se crea el Scanner
 		Scanner sc2 = new Scanner(System.in);
 
+		// Se integra la variable
 		int altura;
 
+		// Se ejecuta el bucle hasta que no se introduzca la altura necesaria
 		do {
 			System.out.println("Introduce la altura de la U: ");
 			altura = sc2.nextInt();
 		} while (altura < 3);
 
+		// Aquí empieza el código necesario para realizar la U con espacios necesarios
+		// de por medio -2 a la altura igual que la base
 		for (int i = altura; i > 1; i--) {
 			System.out.printf("*");
 			for (int j = altura - 2; j > 0; j--) {
@@ -65,15 +76,17 @@ public class Boletin3 {
 		for (int h = altura - 2; h > 0; h--) {
 			System.out.printf("*");
 		}
-
-		sc2.close();
+		
+		sc2.close(); // Se cierra el Scanner
 
 	}
-
+	
+	// Se crea el método estático del ejercicio 3
 	public static void Ejercicio3() {
-
+		// Se crea el Scanner
 		Scanner sc3 = new Scanner(System.in);
-
+		
+		// Se integra la variable
 		int dígito;
 
 	}
--- UD02/src/boletin3/Main.java
+++ UD02/src/boletin3/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-//	Boletin3.Ejercicio1();
+//		Boletin3.Ejercicio1();
 		Boletin3.Ejercicio2();
 	}
 }
